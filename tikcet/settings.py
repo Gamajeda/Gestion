83dcdaf2fc058881e4e1dfff3c6ca6e3c5594f6a
Gamajeda <l20211781@tectijuana.edu.mx>
2024-12-02 00:59:24 -0800

    Agregado rol de administrador y actualizaciones en UserProfile

--- tikcet/settings.py
+++ tikcet/settings.py
@@ -118,7 +118,7 @@ LOGIN_URL = '/login/'
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
